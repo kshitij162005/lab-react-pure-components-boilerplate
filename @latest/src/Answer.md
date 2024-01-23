44fba1021c98daa2d6a82a020bafc35e20f59d75
Kshitij Kotecha <kshitijkotecha@Kshitijs-MacBook-Air.local>
2024-01-23 11:34:05 +0530

    liveDone

---
+++ @latest/src/Answer.md
@@ -0,0 +1,5 @@
+# Difference between Simple Component and Pure Component :- 
+
+# Simple Component - A simple component in React means the basic React.Component. It re-renders whenever it receives new props or state, regardless of if we have done any change. 
+
+# Pure Component - In contrast, a pure component, which is React.PureComponent. It automatically prevents unnecessary renders when props and state remain unchanged and only re-renders when we have done any change in the prop or state. 
---
+++ @latest/src/components/SimpleCounterComponent.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+export default class SimpleCounterComponent extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+      toggle: false
+    };
+  }
+
+  handleToggleClick = () => {
+    this.setState((prevState) => ({ toggle: !prevState.toggle }));
+  };
+
+  handleCounterClick = () => {
+    if (this.state.toggle) {
+      this.setState((prevState) => ({ count: prevState.count + 1 }));
+    }
+  };
+
+  render() {
+    const { count, toggle } = this.state;
+    console.log("this is a Simple Component")
+
+    return (
+      <div>
+        <h2>Simple Component</h2>
+        <h3>Count: {count}</h3>
+        <button onClick={this.handleToggleClick}>Set Toggle</button>
+        <button onClick={this.handleCounterClick}>Counter</button>
+      </div>
+    );
+  }
+}
+
